48beaf809dadf4186a372d6d4590834a851928ce
ndlovuraymond <93596122+ndlovuraymond@users.noreply.github.com>
2022-01-31 00:38:00 +0200

    Add project files.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    { 
+        DateTime NOW = DateTime.Now;
+        string sr = $"{NOW}.txt";
+        using (StreamWriter sw = File.CreateText(sr))
+        {
+            string A;
+            sw.WriteLine("Captains Log");
+            sw.WriteLine("Stardate " + NOW);
+            do
+            {
+                Console.WriteLine(NOW);
+                A = Console.ReadLine();
+                if (A == "start") do { A = Console.ReadLine(); sw.WriteLine(Console.ReadLine()); } 
+                    while (A != "stop");
+            } while (A != "stop");
+            sw.WriteLine("Jean-Luc Pacard");
+
+        }
+    }
+}
+
